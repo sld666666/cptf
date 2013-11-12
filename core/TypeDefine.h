84512f114092c1bcb572c5a6aa6612a32fcd21b3
sld666666 <sld666666@gmail.com>
2013-11-12 23:37:38 +0800

    add wtlDemo

---
+++ core/TypeDefine.h
@@ -0,0 +1,6 @@
+#ifndef CORE_TYPEDEFINE_H
+#define CORE_TYPEDEFINE_H
+
+typedef unsigned long ulong;
+
+#endif
--- core/service/IService.h
+++ core/service/IService.h
@@ -1,13 +1,18 @@
 #ifndef CORE_SERVICE_ISERVICE_H
 #define CORE_SERVICE_ISERVICE_H
+#include "../TypeDefine.h"
+namespace core{
 
-class IService{
+	class IService{
 
-public:
-	IService(){}
-	~IService(){}
+	public:
+		IService(){}
+		virtual ~IService(){}
 
-public:
+	public:
+		virtual	ulong addRef();
+		virtual ulong releaseRef();
+	};
+}
 
-};
 #endif // CORE_SERVICE_ISERVICE_H
